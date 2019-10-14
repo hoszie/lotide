ed32dfd0db71f983eeedf3d9579d750eb4ecf29e
Nick <nhoszko@gmail.com>
2019-10-14 23:25:11 +0000

    using two functions (assertEqual & head)

--- head.js
+++ head.js
@@ -8,8 +8,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, "1");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
+const head = function(arr) {
+  return arr[0];
+};
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "ello");
