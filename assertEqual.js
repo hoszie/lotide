5e8c48f8503330ba2ce0212d266332398142a24c
Nick <nhoszko@gmail.com>
2019-10-14 23:02:51 +0000

    working function, includes emojis

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  const happyEmoji = String.fromCodePoint(128512);
+  const sadEmoji = String.fromCodePoint(128530);
+  if (actual === expected) {
+    console.log(happyEmoji + " Assertion Passed: : [actual] === [expected]");
+  } else {
+    console.log(sadEmoji + " Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, "1");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
