83eff81a228ceb08b2901b9b379cc35ef0636594
Nick <nhoszko@gmail.com>
2019-10-18 02:51:06 +0000

    Found out my node --version because of this. v8.9.4

---
+++ flatten.js
@@ -0,0 +1,30 @@
+const assertArraysEqual = function (arr, arr2) {
+  const happyEmoji = String.fromCodePoint(128512);
+  const sadEmoji = String.fromCodePoint(128530);
+
+  if (eqArrays(arr, arr2)) {
+    console.log(`${happyEmoji} Assertion Passed: ${arr} === ${arr2}`);
+  } else {
+    console.log(`${sadEmoji} Assertion Failed: ${arr} !== ${arr2}`);
+  }
+}
+const eqArrays = function(arr, arrTwo) {
+  if (arr.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arr.length; i ++) {
+    if (arr[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const flatten = function(arr) {
+  let res = arr.flat();
+  return res;
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, [2, 3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3], [4, 5, 6]]),[1, 2, 3, 4, 5, 6]);
